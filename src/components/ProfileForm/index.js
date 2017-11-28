c1467e14ded72c8350d7e1186b9db2fbf5d401d7
Kierancz <kieran.czerwinski@colorado.edu>
2017-11-28 01:04:22 -0700

    simplified form

--- src/components/ProfileForm/index.js
+++ src/components/ProfileForm/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Button from 'material-ui/Button';
-import TextField from 'material-ui/TextField';
 import Dialog, {
   DialogActions,
   DialogContent,
@@ -10,13 +9,15 @@ import Dialog, {
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Radio, { RadioGroup } from 'material-ui/Radio';
-import { FormLabel, FormControl, FormControlLabel, FormHelperText } from 'material-ui/Form';
-import Input, { InputLabel } from 'material-ui/Input';
-import { MenuItem } from 'material-ui/Menu';
-import Select from 'material-ui/Select';
+import { 
+  FormLabel, 
+  FormControl, 
+  FormControlLabel, 
+  FormHelperText } from 'material-ui/Form';
 
 const styles = theme => ({
   root: {
+    left: '0px',
     display: 'flex',
   },
   group: {
@@ -27,27 +28,16 @@ const styles = theme => ({
     flexWrap: 'wrap',
   },
   formControl: {
-    margin: theme.spacing.unit,
-    minWidth: 120,
-    maxWidth: 300,
+    margin: theme.spacing.unit * 3,
   },
 });
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const upperSizes = [
-  'Extra Small',
-  'Small',
-  'Medium',
-  'Large',
-  'Extra Large',
-  'Extra Extra Large'
-];
 
 class ProfileForm extends React.Component {
   state = {
     open: false,
     gender: '',
-    upper: [],
+    upper: '',
+    fit: '',
   };
 
   handleClickOpen = () => {
@@ -62,8 +52,12 @@ class ProfileForm extends React.Component {
     this.setState({ gender });
   };
 
-  handleUpperChange = event => {
-    this.setState({ upper: event.target.value });
+  handleUpperChange = (event, upper) => {
+    this.setState({ upper });
+  };
+
+  handleFitChange = (event, fit) => {
+    this.setState({ fit });
   };
 
   render() {
@@ -71,51 +65,59 @@ class ProfileForm extends React.Component {
 
     return (
       <div>
-        <Button onClick={this.handleClickOpen}>Open form dialog</Button>
+        <Button raised color="primary" onClick={this.handleClickOpen}>Create Fit Profile</Button>
         <Dialog open={this.state.open} onRequestClose={this.handleRequestClose}>
           <DialogTitle>New Fit Profile</DialogTitle>
           <DialogContent>
             <DialogContentText>
               Add your fit profile info below to get clothes that actually fit!
             </DialogContentText>
-            
-            <FormLabel component="legend">Gender</FormLabel>
-            <RadioGroup
-              aria-label="gender"
-              name="gender"
-              className={classes.group}
-              value={this.state.gender}
-              onChange={this.handleGenderChange}
-            >
-              <FormControlLabel value="male" control={<Radio />} label="Male" />
-              <FormControlLabel value="female" control={<Radio />} label="Female" />
-              <FormControlLabel value="other" control={<Radio />} label="Other" />
-            </RadioGroup>
 
-            <InputLabel htmlFor="upper-size">Upper Size</InputLabel>
-            <Select
-              multiple
-              value={this.state.upper}
-              onChange={this.handleUpperChange}
-              input={<Input id="upper-size" />}
-              MenuProps={{
-                PaperProps: {
-                  style: {
-                    maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-                    width: 200,
-                  },
-                },
-              }}
-            >
-              {upperSizes.map(upper => (
-                <MenuItem
-                  key={upper}
-                  value={upper}
-                >
-                  {upper}
-                </MenuItem>
-              ))}
-            </Select>
+            <FormControl component="fieldset" required className={classes.formControl}>
+              <FormLabel component="legend">Gender</FormLabel>
+              <RadioGroup
+                aria-label="gender"
+                name="gender"
+                className={classes.group}
+                value={this.state.gender}
+                onChange={this.handleGenderChange}
+              >
+                <FormControlLabel value="male" control={<Radio />} label="Male" />
+                <FormControlLabel value="female" control={<Radio />} label="Female" />
+                <FormControlLabel value="other" control={<Radio />} label="Other" />
+              </RadioGroup>
+            </FormControl>
+
+            <FormControl component="fieldset" required className={classes.formControl}>
+              <FormLabel component="legend">Upper Size</FormLabel>
+              <RadioGroup
+                aria-label="upper-size"
+                name="upper-size"
+                className={classes.group}
+                value={this.state.upper}
+                onChange={this.handleUpperChange}
+              >
+                <FormControlLabel value="sm" control={<Radio />} label="Small" />
+                <FormControlLabel value="md" control={<Radio />} label="Medium" />
+                <FormControlLabel value="lg" control={<Radio />} label="Large" />
+                <FormControlLabel value="xlg" control={<Radio />} label="Extra Large" />
+              </RadioGroup>
+            </FormControl>
+
+            <FormControl component="fieldset" required className={classes.formControl}>
+              <FormLabel component="legend">Fit Preference</FormLabel>
+              <RadioGroup
+                aria-label="fit"
+                name="fit"
+                className={classes.group}
+                value={this.state.fit}
+                onChange={this.handleFitChange}
+              >
+                <FormControlLabel value="slim" control={<Radio />} label="Slim Fit" />
+                <FormControlLabel value="athletic" control={<Radio />} label="Athletic Fit" />
+                <FormControlLabel value="bigTall" control={<Radio />} label="Big & Tall" />
+              </RadioGroup>
+            </FormControl>
           </DialogContent>
           <DialogActions>
             <Button onClick={this.handleRequestClose} color="primary">
@@ -136,87 +138,3 @@ ProfileForm.propTypes = {
 };
 
 export default withStyles(styles)(ProfileForm);
-
-/*
-class ProfileForm extends React.Component {
-  render() {
-    return (
-      <Form>
-        <Field
-          name="name" 
-          component={TextField} 
-          placeholder="Profile Name"
-        />
-
-        <Section>
-          <InputLabel htmlFor="gender">Gender</InputLabel>
-          <Field
-            name="gender" 
-            component={Select} 
-            placeholder="Gender"
-            inputField={<Input id="gender" value="" />}
-
-          >
-            <MenuItem value="male">Male</MenuItem>
-            <MenuItem value="female">Female</MenuItem>
-            <MenuItem value="nonbinary">Non-binary</MenuItem>
-          </Field>
-
-          <Field 
-            name="waist-size" 
-            component={Select} 
-            placeholder="Waist Size"
-          >
-            <MenuItem value="26">26</MenuItem>
-            <MenuItem value="28">28</MenuItem>
-            <MenuItem value="30">30</MenuItem>
-            <MenuItem value="32">32</MenuItem>
-            <MenuItem value="34">34</MenuItem>
-            <MenuItem value="36">36</MenuItem>
-          </Field>
-
-          <Field 
-            name="length-size" 
-            component={Select} 
-            placeholder="Length Size"
-          >
-            <MenuItem value="26">26</MenuItem>
-            <MenuItem value="28">28</MenuItem>
-            <MenuItem value="30">30</MenuItem>
-            <MenuItem value="32">32</MenuItem>
-            <MenuItem value="34">34</MenuItem>
-            <MenuItem value="36">36</MenuItem>
-          </Field>
-
-          <Field name="upper-size" component={RadioGroup}>
-            <RadioButton value="xsm" label="Extra Small"/>
-            <RadioButton value="sm" label="Small"/>
-            <RadioButton value="md" label="Medium"/>
-            <RadioButton value="lg" label="Large"/>
-            <RadioButton value="xlg" label="Extra Large"/>
-          </Field>
-        </Section>
-      </Form>
-    )
-  }
-};
-
-ProfileForm = reduxForm({
-  form: 'profileForm'
-})(ProfileForm);
-
-export default ProfileForm;
-
-/*
-          <Field
-            name="upper-size" 
-            component={Select}
-            label="Size"
-            placeholder="Uppers Size"
-          >
-            <MenuItem value="sm">Small</MenuItem>
-            <MenuItem value="md">Medium</MenuItem>
-            <MenuItem value="lg">Large</MenuItem>
-            <MenuItem value="xlg">Extra Large</MenuItem>
-          </Field>
-          */
